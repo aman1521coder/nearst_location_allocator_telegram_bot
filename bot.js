a4b84c98b8d577381dd38032b01b3d4b51b2c2e2
aman1521coder <amanesu1521@gmail.com>
2025-09-14 22:22:29 +0300

    keyboard added

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const axios = require('axios');
-const fetch=require('axios')
+const fetch=require('axios');
+const { send } = require('process');
+const { text } = require('stream/consumers');
 const TOkEN=process.env.TOkEN
 const API_URL=`https://api.telegram.org/bot${TOkEN}`
 async function sendMessage(chat_id,msg){ 
@@ -19,4 +21,30 @@ return response.data
     }
 }
 
+async function sendKeboard(){
+  const keyboard = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "gym  ", callback_data: "gym" },
+          { text: "restaurnt ", callback_data: "restuarnt" },
+          { text: "hospital ", callback_data: "hospital" },
+          { text: "police ", callback_data: "police" },
+          { text: "school ", callback_data: "school" },
+          { text: "college ", callback_data: "college" },
+          { text: "library ", callback_data: "library" },
+          { text: "park ", callback_data: "park" },
+          { text: "market ", callback_data: "market" },
+          {text: "mall ", callback_data: "mall"},
+          {text: "bus stop ", callback_data: "bus stop"},
+          {text:"fast food",callback_data:"fast food"}
+        ],
 
+        
+      ],
+    },
+  };
+  await sendMessage(chat_id,"choose one from the keyboard",keyboard)
+
+  
+}
